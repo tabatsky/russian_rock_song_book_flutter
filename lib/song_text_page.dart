78d12dcdbccc3b320d7f42703aed42c9a407f813
Evgeny Tabatsky <e.tabatsky@gmail.com>
2024-01-20 18:27:34 -0800

    song text: match parent

--- lib/song_text_page.dart
+++ lib/song_text_page.dart
@@ -28,37 +28,49 @@ class SongTextPage extends StatelessWidget {
           },
         ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Flexible(child: _makeSongTextView()),
-          ],
-        ),
+      body: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _makeSongTextView(context),
+        ],
       ),
     );
   }
 
-  Widget _makeSongTextView() {
-    return SingleChildScrollView(
-      child: Container(
-        color: Colors.yellow,
-        child: Text(currentSong?.text ?? 'null'),
+  Widget _makeSongTextView(BuildContext context) {
+    return Expanded(
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          double width = constraints.maxHeight;
+          double height = constraints.maxHeight;
+          return SingleChildScrollView(
+            child: Container(
+              constraints: BoxConstraints(minHeight: height, minWidth: width),
+              color: Colors.yellow,
+              child: Wrap(
+                children: [
+                  Text(currentSong?.text ?? 'null'),
+                  Container(
+                    height: 80,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
