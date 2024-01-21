7a222cae795f4af75f87bb5de41139a14aef79ed
Evgeny Tabatsky <e.tabatsky@gmail.com>
2024-01-21 13:11:55 -0800

    menu first version

--- lib/song_list_page.dart
+++ lib/song_list_page.dart
@@ -2,14 +2,29 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:russian_rock_song_book/song.dart';
+import 'package:russian_rock_song_book/song_repository.dart';
 
-class SongListPage extends StatelessWidget {
+class SongListPage extends StatefulWidget{
 
   String currentArtist;
   List<Song> currentSongs;
   void Function(Song s) onSongClick;
 
   SongListPage(this.currentArtist, this.currentSongs, this.onSongClick, {super.key});
+
+  @override
+  State<SongListPage> createState() => SongListPageState();
+}
+
+class SongListPageState extends State<SongListPage> {
+
+  List<String> allArtists = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initArtists();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,35 +36,10 @@ class SongListPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(currentArtist),
+        title: Text(widget.currentArtist),
       ),
       drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-          ],
-        ),
+        child: _makeMenuListView(),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -77,16 +67,58 @@ class SongListPage extends StatelessWidget {
     );
   }
 
+  ListView _makeMenuListView() => ListView.builder(
+      padding: EdgeInsets.zero,
+      itemCount: allArtists.length + 1,
+      itemBuilder: (BuildContext context, int index) {
+        if (index == 0) {
+          return const SizedBox(
+            height: 120,
+            child: DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              margin: EdgeInsets.zero,
+              child: Text('Меню'),
+            ),
+          );
+        } else {
+          final artist = allArtists[index - 1];
+          return ListTile(
+            title: Text(artist),
+            onTap: () {
+              log("$artist click");
+            },
+          );
+        }
+      }
+  );
+
+  /*=> ListView(
+    // Important: Remove any padding from the ListView.
+    padding: EdgeInsets.zero,
+    children: [
+
+      ListTile(
+        title: const Text('Item 2'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+    ],
+  );
+*/
   ListView _makeTitleListView() => ListView.builder(
       padding: const EdgeInsets.all(8),
-      itemCount: currentSongs.length,
+      itemCount: widget.currentSongs.length,
       itemBuilder: (BuildContext context, int index) {
-        var song = currentSongs[index];
+        var song = widget.currentSongs[index];
         return Column(
           children: [
             GestureDetector(
               onTap: () {
-                onSongClick(song);
+                widget.onSongClick(song);
               },
               child: Container(
                   height: 50,
@@ -105,4 +137,12 @@ class SongListPage extends StatelessWidget {
       }
   );
 
+  Future<void> _initArtists() async {
+    await SongRepository().initDB();
+    final artists = await SongRepository().getArtists();
+    log(artists.toString());
+    setState(() {
+      allArtists = artists;
+    });
+  }
 }
