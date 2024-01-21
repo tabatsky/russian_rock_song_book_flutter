2850bb8b7d34baebd0b61c60b61ca61fd09c6f91
Evgeny Tabatsky <e.tabatsky@gmail.com>
2024-01-20 18:28:12 -0800

    song list: drawer

--- lib/song_list_page.dart
+++ lib/song_list_page.dart
@@ -23,6 +23,34 @@ class SongListPage extends StatelessWidget {
         // the App.build method, and use it to set our appbar title.
         title: Text(currentArtist),
       ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
