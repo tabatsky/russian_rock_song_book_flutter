801bcbdb461f4ad2e1a22b7cbebfa5861f480ab1
tabatsky <e.tabatsky@gmail.com>
2024-12-01 08:27:30 +1000

    add artist stub for iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "jatx.flutter.russian_rock_song_book/channel",
+                                                    binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        result(["artist", "title1", "text1", "title2", "text2"])
+    })
+      
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
